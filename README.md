6742099d764331aaa82d85adf68f28e1aa44ad48
Weimin Yang <yangwm1983@gmail.com>
2019-06-02 23:27:11 -0700

    complete update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Word-Guess-Game
+    I use a function to replace a element in a string.  At the begining I am so confused that I can not replace it as an array.  And I googled it and found this good function.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,45 +11,84 @@ var animalindex=0;
 var wordindex=0;
 var currentWord=document.getElementById("currentword");
 var wins=document.getElementById("wins");
+var numberRemaining=document.getElementById("numberremaining");
+var letters=document.getElementById("letters");
+letters.textContent = " ";
 var animal=animals[animalindex];
-wins = 0;
+var guessedWord="";
+wins.textContent = 0;
+var isNewAnimal = true;
 
-
-
+function reaplceChartAtPos(ori, index, replacement) {
+    return ori.substr(0, index) + replacement+ ori.substr(index + replacement.length);
+}
 
             document.onkeyup = function(event){
-                if(currentWord.textContent!==animal)
+                if(isNewAnimal)
                 {
+                    if (animalindex === animals.length) {
+                        animalindex=0;
+                    }
+                    guessedWord="";
                     animal=animals[animalindex];
-                    currentWord.textContent=" ";
-                    for(i=0;i<animal.length;i++)
+                    currentWord.textContent="";
+                    for(var i=0;i<animal.length;i++)
                     {
-                        currentWord.textContent+="_ ";
+                        
+                        guessedWord+="_";
                     }
+                    for(i=0;i<guessedWord.length;i++)
+                    {
 
+                        currentWord.textContent+=(guessedWord[i]+" ");
+                    }
+                    isNewAnimal = false;
                 }
-                else{
-
-                }
+                numberRemaining.textContent-=1;
+              
                 var userInput=event.key;
-                for (let i = 0; i < animal.length; i++) 
+                
+                currentWord.textContent="";
+                for (var i = 0; i < animal.length; i++) 
                 {
-                    letter=animal[i];
-                    if(userInput===letter)
-                    {
-                      currentWord.textContent[i]=letter+" ";
-                    }
+                    
+                    if(userInput===animal[i]){
+                        guessedWord = reaplceChartAtPos(guessedWord, i,animal[i]);
+                    }  
+                    currentWord.textContent+=(guessedWord[i]+" ");
                 }
-                if(currentWord.textContent===animal)
+
+                var isNewLetter=true;
+                for(var i=0;i<letters.textContent.length;i++)
+                {
+                    if(letters.textContent[i]===userInput)
+                    isNewLetter=false;
+                }
+                if(isNewLetter)
+                {
+                    letters.textContent+=userInput+"  ";
+                }
+
+                if(guessedWord===animal)
                 {
                     animalindex++;
-
-
+                    isNewAnimal =true;
+                    wins.textContent=parseInt(wins.textContent) + 1;
+                    numberRemaining.textContent=12;
+                    currentWord.textContent="_ _ _ _ _ _ _";
+                    letters.textContent="";
+                }
+                else if(parseInt(numberRemaining.textContent)===0){
+                    alert("You loss");
+                    animalindex++;
+                    isNewAnimal =true;
+                    numberRemaining.textContent=12;
+                    currentWord.textContent="_ _ _ _ _ _ _";
+                    letters.textContent="";
                 }
                            
                 
-                console.log(currentWord.textContent);
-                                          
+                
             }
     
     
